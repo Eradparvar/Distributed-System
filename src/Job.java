3da8ff08eff711e4d254686e6fe5a49d0a902a0b
Eli <eradparv@student.touro.edu>
2018-04-04 23:43:33 -0400

    added code to Job class

--- src/Job.java
+++ src/Job.java
@@ -2,8 +2,19 @@
 public class Job {
     private int jobLengthTime;
 
-    public Jobs(int jobLengthTime) {
+    public Job(int jobLengthTime) {
 	this.jobLengthTime = jobLengthTime;
+    }
+    
+    public void start() {
+	try {
+	    Thread.sleep(jobLengthTime);
+	} catch (InterruptedException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	
+	
     }
 
 }
